ac4d2f7c822dd3d74f8fe20149d57362aa5fe438
arthurdlc <delacour.a1109@gmail.com>
2025-01-17 11:44:19 +0100

    cd

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; // Nécessaire pour recharger une scène
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,29 +7,47 @@ public class PlayerController : MonoBehaviour
     public Rigidbody rb;
     public int health = 5;
     private int score = 0;
+
+    void Update()
+    {
+        // Vérifie si la santé est à 0
+        if (health <= 0)
+        {
+            Debug.Log("Game Over!");
+
+            // Recharge la scène actuelle
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        // Check if the object is tagged as "Pickup"
+        // Vérifie si l'objet a le tag "Pickup"
         if (other.CompareTag("Pickup"))
         {
             score++;
             Debug.Log("Score: " + score);
             other.gameObject.SetActive(false);
         }
+
+        // Vérifie si l'objet a le tag "Trap"
         if (other.CompareTag("Trap"))
         {
             health--;
             Debug.Log("Health: " + health);
-            // if (health <= 0)
-            // {
-            //     Debug.Log("Game Over");
-            //     Application.Quit();
-            // }
+        }
+
+        // Vérifie si l'objet a le tag "Goal"
+        if (other.CompareTag("Goal"))
+        {
+            Debug.Log("You Win!");
+            Application.Quit();
         }
     }
-        
+
     void FixedUpdate()
     {
+        // Gère les entrées pour déplacer le joueur
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
